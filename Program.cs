cc18fabd9ba742434167913786c4292cef91348d
yepIwt <ReetRay@protonmail.com>
2024-10-05 11:15:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HeroFactoryASB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
